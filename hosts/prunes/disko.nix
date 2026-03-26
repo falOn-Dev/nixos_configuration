528e1863bb0bd46a56bc1bee5a6fdf14a7d25f98
Falon Clark <falon.n.clark@gmail.com>
2026-03-26 12:37:07 -0400

    Switch disko to device id instead of /dev subdir

--- hosts/prunes/disko.nix
+++ hosts/prunes/disko.nix
@@ -1,57 +1,57 @@
 { ... }:
 
 {
-    flake.diskoConfigurations.tatiman-prunes = { ... }: {
-        disko.devices = {
-            disk = {
-                system = {
-                    device = "/dev/nvme0n1";
-                    type = "disk";
+  flake.diskoConfigurations.tatiman-prunes = { ... }: {
+    disko.devices = {
+      disk = {
+        system = {
+          device = "/dev/disk/by-id/nvme-KBG40ZNS256G_NVMe_KIOXIA_256GB_X9RPC67PPTLL";
+          type = "disk";
 
-                    content = {
-                        type = "gpt";
-                        partitions = {
-                            ESP = {
-                                size = "512M";
-                                type = "EF00";
-                                content = {
-                                    type = "filesystem";
-                                    format = "vfat";
-                                    mountpoint = "/boot";
-                                };
-                            };
-
-                            root = {
-                                size = "100%";
-                                content = {
-                                    type = "filesystem";
-                                    format = "ext4";
-                                    mountpoint = "/";
-                                };
-                            };
-                        };
-                    };
+          content = {
+            type = "gpt";
+            partitions = {
+              ESP = {
+                size = "512M";
+                type = "EF00";
+                content = {
+                  type = "filesystem";
+                  format = "vfat";
+                  mountpoint = "/boot";
                 };
+              };
 
-                data = {
-                    device = "/dev/sda"; # your 1TB drive
-                        type = "disk";
-
-                    content = {
-                        type = "gpt";
-                        partitions = {
-                            data = {
-                                size = "100%";
-                                content = {
-                                    type = "filesystem";
-                                    format = "ext4";
-                                    mountpoint = "/data";
-                                };
-                            };
-                        };
-                    };
+              root = {
+                size = "100%";
+                content = {
+                  type = "filesystem";
+                  format = "ext4";
+                  mountpoint = "/";
                 };
+              };
             };
+          };
         };
+
+        data = {
+          device = "/dev/disk/by-id/ata-TOSHIBA_MQ04ABF100_X9HNTDKKT";
+          type = "disk";
+
+          content = {
+            type = "gpt";
+            partitions = {
+              data = {
+                size = "100%";
+                content = {
+                  type = "filesystem";
+                  format = "ext4";
+                  mountpoint = "/data";
+                };
+              };
+            };
+          };
+        };
+      };
     };
+  };
 }
