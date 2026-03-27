6daa42766f3365ea7bff44b39760f849687a44d4
falOn-Dev <me@falon.dev>
2026-03-27 01:54:29 +0000

    add bluetio as a default pkg

--- home/falon/default.nix
+++ home/falon/default.nix
@@ -1,8 +1,13 @@
 { ... }:
 {
-    flake.homeModules.home-falon-base = { ... }: {
+    flake.homeModules.home-falon-base = { pkgs, ... }: {
         home.username = "falon";
         home.homeDirectory = "/home/falon";
         home.stateVersion = "25.11";
+
+        home.packages = with pkgs; [
+            bluetui
+        ];
+
     };
 }
