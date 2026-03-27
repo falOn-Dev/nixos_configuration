e89ddfd6bb40ab21c650f5e04298ba258f8d0bd6
falOn-Dev <me@falon.dev>
2026-03-27 01:54:42 +0000

    update niri config

--- home/falon/niri.nix
+++ home/falon/niri.nix
@@ -28,10 +28,45 @@
                 gaps = 6;
             };
 
+            outputs."eDP-1" = {
+                mode = {
+                    height = 1080;
+                    width = 1920;
+                    refresh = 60.0;
+                };
+                position = {
+                    x = 0;
+                    y = 0;
+                };
+            };
+
+            outputs."HDMI-A-2" = {
+                mode = {
+                    height = 1080;
+                    width = 1920;
+                    refresh = 60.0;
+                };
+                position = {
+                    x = 1920;
+                    y = 0;
+                };
+                scale = 0.75;
+            };
+
             binds = {
                 "Mod+Return".action.spawn = "ghostty";
-                "Mod+Space".action.spawn-sh = [ "rofi" "-show" "drun" ];
+                "Mod+Space".action.spawn = [ "rofi" "-show" "drun" ];
                 "Mod+Q".action.close-window = [];
+
+                "Mod+H".action.focus-column-left = [];
+                "Mod+L".action.focus-column-right = [];
+                "Mod+J".action.focus-window-or-workspace-down = [];
+                "Mod+K".action.focus-window-or-workspace-up = [];
+
+                "Mod+O".action.toggle-overview = [];
+                "Mod+F".action.maximize-column = [];
+
+                "Mod+Shift+H".action.show-hotkey-overlay = [];
             };
         };
     };
