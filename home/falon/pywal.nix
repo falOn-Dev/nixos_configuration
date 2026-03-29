7e9dc66ffcfda4704b0df82fa2532996548e37c4
falOn-Dev <me@falon.dev>
2026-03-29 21:57:55 +0000

    Add pywalfox script

--- home/falon/pywal.nix
+++ home/falon/pywal.nix
@@ -3,6 +3,7 @@
     flake.homeModules.home-falon-pywal = { pkgs, ... }: {
         home.packages = with pkgs; [
             pywal16
+            pywalfox-native
             imagemagick
         ];
     };
